3b20cf0b9359a11250bc9ff365a5820c48afdb4a
Eduardo Zago <zagoe22@gmail.com>
2023-11-28 08:52:01 -0300

    fixed sequencer working

---
+++ fixed-sequencer/fixed-sequencer.py
@@ -0,0 +1,76 @@
+import queue
+import random
+from datetime import datetime
+
+def message_generator():
+    # random message generator
+    content_message = random.randint(10, 99)
+    
+    # timestamp with a random delay
+    timestamp_message = random.randint(1, 100) * random.random()#datetime.now().timestamp() * random.random()
+    message = (content_message, round(timestamp_message, 3))
+
+    return message
+
+def process_message(message):
+    print(message)
+
+# task creator
+def task(queue, message):
+    queue.put(message)
+
+# get queue
+def get_queue(queue):
+    messages = []
+
+    while not queue.empty():
+        msg = queue.get()
+        messages.append(msg)
+
+    return messages
+
+# creating some tasks
+def sender(queue, num_tasks):
+    print("Sender Tasks")
+
+    for i in range(num_tasks):
+        message = message_generator()
+        task(queue, message)
+        print(f"Task {i}: {message}")
+
+def sequencer(queue):
+    messages = []
+
+    # get queue
+    messages = get_queue(queue)
+
+    messages_sorted = sorted(messages, key=lambda x: x[1])
+
+    print("\nSequencer Tasks")
+    for i in range(len(messages_sorted)):
+        task(queue, messages_sorted[i])
+        print(f"Task {i}: {messages_sorted[i]}")
+
+def receiver(queue):
+    messages = get_queue(queue)
+
+    print("\nReceiver Tasks")
+
+    for i in range(len(messages)):
+        print(f"Processsing task {i}:")
+        process_message(messages[i])
+
+def main():
+
+    messages_queue = queue.Queue()
+
+    num_tasks = 10
+
+    sender(messages_queue, num_tasks)
+
+    sequencer(messages_queue)
+
+    receiver(messages_queue)
+
+if __name__ == "__main__":
+    main()
--- fixed-sequencer/readme.md
+++ fixed-sequencer/readme.md
@@ -1,2 +1,20 @@
-## Atomic Broadcast with Fixed Sequencer
+## Group Communication with Fixed Sequencer
 
+Group communication using tasks and message queue
+
+The algorithm works as follows:
+1. Sender group 
+    i. Tasks generate a message that is placed on the queue
+    ii. Messages are randomly generated numbers with a time they were generated (here the timestamp generation was simulated for better visualization)
+
+2. Sequencer 
+    i. After the sender group sends the messages, they are received in the sequencer
+    ii. The sequencer orders messages according to the timestamp at which the message was created
+
+2. Receiver group 
+    i. The receiver group receives the messages and processes them as ordered by the sequencer
+
+To execute:
+```sh
+python3 fixed_sequencer.py
+```
